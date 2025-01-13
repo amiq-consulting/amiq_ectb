3d245a38038475935b3901de3d05c3d353df0e19
Andrei Blindu <andrei.blindu@amiq.com>
2025-01-13 13:54:15 +0200

    Changed reg functions to display default values when plusarg is not found.

--- sv/amiq_ectb_reg_functions.svh
+++ sv/amiq_ectb_reg_functions.svh
@@ -6,11 +6,9 @@ function int int_reg(string my_var_name, int default_value=0);
     my_var_name = {get_name(), "_", my_var_name};
     `uvm_info(get_name(), $sformatf("Looking for var:%s", my_var_name),UVM_NONE)
     if(!$value$plusargs({my_var_name, "=%0d"}, int_reg)) begin
-        `uvm_info(get_name(), $sformatf("Didn't find the plusarg"),UVM_NONE)
         int_reg = default_value;
-    end else begin
-        `uvm_info(get_name(), $sformatf("Found the plusarg"),UVM_NONE)
     end
+    `uvm_info(get_name(), $sformatf("+%s=%0d", my_var_name, int_reg),UVM_NONE)
 endfunction
 
 function bit bit_reg(string my_var_name, bit default_value=0);
@@ -18,11 +16,10 @@ function bit bit_reg(string my_var_name, bit default_value=0);
     my_var_name = {get_name(), "_", my_var_name};
     `uvm_info(get_name(), $sformatf("Looking for var:%s", my_var_name),UVM_NONE)
     if(!$value$plusargs({my_var_name, "=%0b"}, bit_reg)) begin
-        `uvm_info(get_name(), $sformatf("Didn't find the plusarg"),UVM_NONE)
         bit_reg = default_value;
-    end else begin
-        `uvm_info(get_name(), $sformatf("Found the plusarg"),UVM_NONE)
     end
+    `uvm_info(get_name(), $sformatf("+%s=%0b", my_var_name, bit_reg),UVM_NONE)
+    
 endfunction
 
 function string string_reg(string my_var_name, string default_value="");
@@ -30,9 +27,7 @@ function string string_reg(string my_var_name, string default_value="");
     my_var_name = {get_name(), "_", my_var_name};
     `uvm_info(get_name(), $sformatf("Looking for var:%s", my_var_name),UVM_NONE)
     if(!$value$plusargs({my_var_name, "=%0s"}, string_reg)) begin
-        `uvm_info(get_name(), $sformatf("Didn't find the plusarg"),UVM_NONE)
         string_reg = default_value;
-    end else begin
-        `uvm_info(get_name(), $sformatf("Found the plusarg"),UVM_NONE)
-    end
+	end
+    `uvm_info(get_name(), $sformatf("+%s=%s", my_var_name, string_reg),UVM_NONE)
 endfunction
